7bf910b5296b99499c55b962a6642f85255b20ae
pomaxa399 <pomaxa399@gmail.com>
2019-07-25 02:26:50 +0300

    post_detail work

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -9,3 +9,8 @@ posts = Blueprint('posts', __name__, template_folder='templates')
 def index():
     posts = Post.query.all()
     return render_template('posts/index.html', posts=posts)
+
+@posts.route('/<slug>')
+def post_detail(slug):
+    post = Post.query.filter(Post.slug==slug).first()
+    return render_template('posts/post_detail.html', post=post)
